ff4aa1258fc5d149e0e15630dd4d6318f69700a8
DevarapalliSampreeth <114420099+DevarapalliSampreeth@users.noreply.github.com>
2022-10-17 10:21:30 +0530

    class_assignment

---
+++ csharp/class_Assignment/class_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int Total;
+            int Sum = 0;
+            int Value;
+            float Average;
+            Console.WriteLine("enter total count of matches : ");
+            Total = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < Total; i++)
+            {
+                Console.WriteLine("enter score: ");
+                Value = Convert.ToInt32(Console.ReadLine());
+                Sum += Value;
+            }
+            Average = (float)Sum / Total;
+            Console.WriteLine("Sum: " + Sum + ", Average: " + Average);
+        }
+    }
+}
